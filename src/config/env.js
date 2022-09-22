bf74c2d6c2f16a27ede42fa702063370c4ee58d2
asasia1935 <88706217+asasia1935@users.noreply.github.com>
2022-09-23 05:08:31 +0900

    Revert "Kim"

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,15 @@
-export const API_HOST = "http://ufriends.today";
-export const API_LOGIN = "http://ufriends.today/api/member/login";
-export const API_EMAILVALID = "http://ufriends.today/api/member/emailValid";
-export const API_NICKNAME = "http://ufriends.today/api/member/nickValid";
-export const API_JOIN = "http://ufriends.today/api/member/join";
+// release
+
+// export const API_HOST = "http://ufriends.today";
+// export const API_LOGIN = "http://ufriends.today/api/member/login";
+// export const API_EMAILVALID = "http://ufriends.today/api/member/emailValid";
+// export const API_NICKNAME = "http://ufriends.today/api/member/nickValid";
+// export const API_JOIN = "http://ufriends.today/api/member/join";
+
+// dev
+
+export const API_HOST = "http://localhost:8080";
+export const API_LOGIN = API_HOST + "/api/member/login";
+export const API_EMAILVALID = API_HOST + "/api/member/emailValid";
+export const API_NICKNAME = API_HOST + "/api/member/nicknameValid";
+export const API_JOIN = API_HOST + "/api/member/join";
