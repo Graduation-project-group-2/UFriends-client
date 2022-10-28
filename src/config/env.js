7ae357aa78641cbe48e5c2b737c562f4b3b5622c
danpung2 <danpung_2@naver.com>
2022-10-28 14:15:15 +0900

    env release

--- src/config/env.js
+++ src/config/env.js
@@ -1,14 +1,10 @@
 // release
 
-// export const API_HOST = "http://ufriends.today";
-// export const API_LOGIN = "http://ufriends.today/api/member/login";
-// export const API_EMAILVALID = "http://ufriends.today/api/member/emailValid";
-// export const API_NICKNAME = "http://ufriends.today/api/member/nickValid";
-// export const API_JOIN = "http://ufriends.today/api/member/join";
-
+export const API_HOST = "http://www.ufriends.today";
 // dev
 
-export const API_HOST = "http://localhost:8080";
+// export const API_HOST = "http://localhost:8080";
+
 export const API_LOGIN = API_HOST + "/api/member/login";
 export const API_EMAIL_VALID = API_HOST + "/api/member/emailValid";
 export const API_NICKNAME_VALID = API_HOST + "/api/member/nicknameValid";
