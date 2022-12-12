b3e9c2f820f328c4538e3065052e759806aab17a
asasia1935 <sungmin1935@naver.com>
2022-12-12 12:39:23 +0900

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,11 +1,6 @@
 // release
 
-// export const API_HOST = "http://ufriends.today";
-// export const API_LOGIN = "http://ufriends.today/api/member/login";
-// export const API_EMAILVALID = "http://ufriends.today/api/member/emailValid";
-// export const API_NICKNAME = "http://ufriends.today/api/member/nickValid";
-// export const API_JOIN = "http://ufriends.today/api/member/join";
-
+//export const API_HOST = "http://www.ufriends.today";
 // dev
 
 export const API_HOST = "http://localhost:8080";
