2d9b9988e64952248ff78d4be438785c6fe5ea66
LeeHajeong <hjpurege@gachon.ac.kr>
2023-05-24 14:35:57 +0900

    fix: Chatbot page

--- src/routes/Chatbot.js
+++ src/routes/Chatbot.js
@@ -3,12 +3,25 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function Chatbot() {
     return (
-        <div>
-            <div>
+        <div className="entireDiv">
+            <div className="androidDiv">
                 <Header/>
-                <div>
-                    {/* 대화 부분 */}
+                <div className = "chattingDiv">
+                    <p className="ufriendTextField" id="ufriend_text" type="text">안녕?</p>
+                    <p className="userTextField" id="user_text" type="text">나 오늘 완전 기분 안 좋은 일 있었어ㅠㅠ</p>
+                    <p className="ufriendTextField" id="ufriend_text" type="text">무슨 일이야?</p>
+                    <p className="userTextField" id="user_text" type="text">....</p>
                 </div>
+
+                <div class = "sendFormDiv">
+                    <input id="input_send_Message" type="text" />
+                    <button id="submit_button" type="submit">전송</button>
+                </div>
+
+
+
+
+
                 <div>
                     <div class="input-group">
                         <ul className="list-group">
@@ -16,13 +29,12 @@ function Chatbot() {
 
                             {/*</li>*/}
                         </ul>
-                        <input type="text" class="form-control"/>
+                        
+                        
+                        {/* <input type="text" class="form-control"/>
                         <div class="input-group-append">
                             <button class="btn btn-primary" type="button">보내기</button>
-                        </div>
-                        {/*/!* 입력창 부분 *!/*/}
-                        {/*<input type="text"/>*/}
-                        {/*<button type="submit">전송</button>*/}
+                        </div> */}
                     </div>
                 </div>
             </div>
